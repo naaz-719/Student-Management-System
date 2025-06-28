0cbe0ea97656263a51a8e60444aa88f1cef2eeb3
naaz-719 <naazmulla9195@gmail.com>
2025-06-28 21:41:07 +0400

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "Naaz@1234";
-$dbname = "student_system";
+$password = "";
+$dbname = "";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
